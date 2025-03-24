09c654c57ac3cca4543c9012aff78b5d54da7f76
Eugen Neuber <eugen.neuber@tugraz.at>
2025-03-24 17:05:28 +0100

    Add automatic import into typesense to the command.
    Set the alias to the current imported collection.
    Remove obsolete collections.

--- src/Command/GenerateActivitiesCommand.php
+++ src/Command/GenerateActivitiesCommand.php
@@ -6,10 +6,12 @@ namespace Dbp\Relay\NexusBundle\Command;
 
 use Dbp\Relay\NexusBundle\Service\ConfigurationService;
 use Dbp\Relay\NexusBundle\Typesense\Connection;
+use Http\Client\Exception;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Typesense\Client;
+use Typesense\Exceptions\TypesenseClientError;
 
 class GenerateActivitiesCommand extends Command
 {
@@ -85,14 +87,12 @@ class GenerateActivitiesCommand extends Command
         if (count($data) > 0) {
             if ($verbose) {
                 $output->writeln('<info>Document to upsert</info>');
-            }
-            if ($verbose) {
                 $output->writeln(json_encode($data, JSON_PRETTY_PRINT | JSON_THROW_ON_ERROR));
             }
 
             $schema = json_decode(file_get_contents(__DIR__.'/../../data-definition/schema.json'), true, 16);
 
-            $schema['name'] = $collectionName = 'nexus--'.date('Ymd-His');
+            $schema['name'] = $collectionName = self::collectionPrefix().date('Ymd-His');
             $this->client->collections->create($schema);
             $this->client->collections[$collectionName]->documents->import($data, ['action' => 'upsert']);
 
@@ -111,6 +111,11 @@ class GenerateActivitiesCommand extends Command
                     $output->writeln('<info>Aliases written</info>');
                     $output->writeln("alias {$this->aliasName} for $collectionName");
                 }
+                $removed = $this->removeOldCollections();
+                if ($verbose) {
+                    $output->writeln('<info>Remove old collections</info>');
+                    $output->writeln("{$removed} old collections removed");
+                }
             } else {
                 $output->writeln('<error>Upsert documents failed.</error>');
 
@@ -125,11 +130,60 @@ class GenerateActivitiesCommand extends Command
         return self::SUCCESS;
     }
 
+    /**
+     * Remove collections, which names start with self::collectionPrefix(), but keep at least some.
+     *
+     * @param int $keep number of collections to keep
+     * @return int number of collections deleted
+     * @throws Exception
+     * @throws TypesenseClientError
+     */
+    private function removeOldCollections(int $keep = 3): int
+    {
+        $removed = 0;
+        $nexusCollectionNames = [];
+        $collections = $this->client->collections->retrieve();
+        foreach ($collections as $collection) {
+            $name = $collection['name'];
+            echo "name: $name\n";
+            if (str_starts_with($name, self::collectionPrefix())) {
+                $nexusCollectionNames[] = $name;
+            }
+        }
+        if (count($nexusCollectionNames) > 0) {
+            sort($nexusCollectionNames);
+            foreach ($nexusCollectionNames as $index => $name) {
+                if ($keep <= $index) {
+                    $this->client->collections[$name]->delete();
+                    ++$removed;
+                }
+            }
+        }
+
+        return $removed;
+    }
+
+    /**
+     * Get the last part of the URL when separated by slashes.
+     *
+     * @param string $url
+     * @return string
+     */
     private static function last(string $url): string
     {
         $parts = explode('/', $url);
         $parts = array_reverse($parts);
 
         return $parts[0] ?? '';
+    }
+
+    /**
+     * Get the collection prefix for all collections created for this bundle.
+     *
+     * @return string
+     */
+    private static function collectionPrefix(): string
+    {
+        return 'nexus--';
     }
 }
