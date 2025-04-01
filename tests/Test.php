a4841dfa99d0fe9b9d30036efa7ae9d20ff4f31c
Christoph Reiter <reiter.christoph@gmail.com>
2025-04-01 12:15:17 +0200

    linter

--- tests/Test.php
+++ tests/Test.php
@@ -10,9 +10,9 @@ class Test extends KernelTestCase
 {
     public function testContainer()
     {
-        //self::bootKernel();
-        //$container = static::getContainer();
-        //$this->assertNotNull($container);
+        // self::bootKernel();
+        // $container = static::getContainer();
+        // $this->assertNotNull($container);
         $this->expectNotToPerformAssertions();
     }
 }
