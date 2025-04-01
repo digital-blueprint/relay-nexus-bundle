93f2d3cb452063c20e58c869279a9fa8ed015160
Christoph Reiter <reiter.christoph@gmail.com>
2025-04-01 15:43:05 +0200

    authz: fix missing bundle config setup
    otherwise the role is not defined

--- src/DependencyInjection/DbpRelayNexusExtension.php
+++ src/DependencyInjection/DbpRelayNexusExtension.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Dbp\Relay\NexusBundle\DependencyInjection;
 
 use Dbp\Relay\CoreBundle\Extension\ExtensionTrait;
+use Dbp\Relay\NexusBundle\Authorization\AuthorizationService;
 use Dbp\Relay\NexusBundle\Service\ConfigurationService;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -28,6 +29,9 @@ class DbpRelayNexusExtension extends ConfigurableExtension
         $this->addRouteResource($container, __DIR__.'/../Resources/config/routes.yaml', 'yaml');
 
         $definition = $container->getDefinition(ConfigurationService::class);
+        $definition->addMethodCall('setConfig', [$mergedConfig]);
+
+        $definition = $container->getDefinition(AuthorizationService::class);
         $definition->addMethodCall('setConfig', [$mergedConfig]);
     }
 }
